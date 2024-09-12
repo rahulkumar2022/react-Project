28b86396320a62bf5e8d70fb3185ade4190b95a4
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2024-09-13 00:48:24 +0530

    day 1 basics

---
+++ start-02/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hii hello world</h1>
+    </div>
+  );
+}
+
+export default App;
